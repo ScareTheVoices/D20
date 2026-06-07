eece3f98d6abfeac2a87ae1006d588903696df0c
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-06-07 19:13:38 -0400

    Add campaign information for 'Authority Over All'

---
+++ D20-Official Campaigns/AuthorityOverAll/info.js
@@ -0,0 +1,52 @@
+const campaignInfo = {
+    // Basic information
+    title: "Authority Over All",
+    subtitle: "'Beware Those Unfamiliar To Us.'",
+    description: "An epic campaign that takes you all around discovering what lies beyond Embres.",
+    
+    // Campaign metadata
+    campaignLevel: "1-70",
+    players: "3-6",
+    duration: "1+",
+    difficulty: "Medium",
+    
+    // Banner image URL
+    banner: "https://raw.githubusercontent.com/ScareTheVoices/D20/main/D20-Official%20Campaigns/AuthorityOverAll/banner.png",
+    
+    // Story section (can include HTML)
+    story: `
+        <h3> Campaign Overview</h3>
+        <p>Venture Away From Embres.
+        After the Events of Ashes Of War Trade routes need to be established as Embres is the leading power in Promethia, now being protected and ruled by Grant The Pheonix Emperor. Travel away from Promethia and learn what lies away from the land of Fire by visiting other lands whilst visiting the shrines of the 14 Fundamentals. Beware the one who seeks control as they wish to be the Authority Over All.</p>
+        
+        `,
+    
+    // Key features
+    features: [
+        "Elemental-themed magic and enemies",
+        "Expansive world with unique regions",
+        "Political Trade",
+        "Side-quests for continent or body of land",
+    ],
+    
+    // Campaign structure
+    chapters: [
+       "Tidas",
+    ],
+    
+    // Resources and downloads
+    resources: [
+        {
+            name: "Play",
+            link: "https://scarethevoices.github.io/D20/D20-Official%20Campaigns/AshesOfWar/D20.html"
+        }
+    ]
+};
+
+window.campaignInfo = campaignInfo;
+
+
+
+
+
+
